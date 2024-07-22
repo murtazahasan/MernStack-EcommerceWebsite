520cfe139b64a1e22bf9e22f187ee23cda0ca895
murtazahasan <murtazahasan276@gmail.com>
2024-07-22 23:09:56 +0500

    ..

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 const router = express.Router();
 import { createOrder, getAllOrders } from "../controllers/order.controller.js";
-import { verifyToken } from "../middlewares/auth.middleware.js";
+import { verifyToken, verifyAdmin } from "../middlewares/auth.middleware.js";
 
 // Create new order (requires authentication)
 router.post("/new-order", verifyToken, createOrder);
 
 // Get all orders (for admin panel, requires authentication)
-router.get("/all", verifyToken, getAllOrders);
+router.get("/all", verifyToken, verifyAdmin, getAllOrders);
 
 export default router;
